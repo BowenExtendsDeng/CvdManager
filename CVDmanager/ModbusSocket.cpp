688442f7b010100e7c6b34f8f77feb549312d70c
bowen <2631702650@qq.com>
2022-10-30 11:25:49 +0800

    添加了excel读取模块

--- CVDmanager/ModbusSocket.cpp
+++ CVDmanager/ModbusSocket.cpp
@@ -1,30 +1,21 @@
 ﻿#include "ModbusSocket.h"
 
-ModbusSocket::ModbusSocket(unsigned char m_1, unsigned char m_2, unsigned char m_3,
-	unsigned char m_4, unsigned char m_7, unsigned char m_data_addr, 
-	unsigned char m_data_num) {
+ModbusSocket::ModbusSocket(std::string device_name) {
 
 	// read from config
 	CMyIni reader;
 	reader.ReadIni("config.ini");
-	port = atoi(reader.GetValue("cvd_machine", "port").c_str());
+	port = atoi(reader.GetValue(device_name, "port").c_str());
 	ip = QString::fromStdString(reader.GetValue("cvd_machine", "ip"));
 	
 	Logger::Log("try to connect cvd server with param ip="
-		+ reader.GetValue("cvd_machine", "ip")
+		+ reader.GetValue(device_name, "ip")
 		+ " port= "
-		+ reader.GetValue("cvd_machine", "port"), Logger::INFO);
+		+ reader.GetValue(device_name, "port"), Logger::INFO);
 	
 	socket = new QTcpSocket();
 	socket->connectToHost(ip, port, QTcpSocket::ReadWrite);
 
-	this->m_1 = m_1;
-	this->m_2 = m_2;
-	this->m_3 = m_3;
-	this->m_4 = m_4;
-	this->m_7 = m_7;
-	this->m_data_addr = m_data_addr;
-	this->m_data_num = m_data_num;
 
 	m_data_addr = ((m_data_addr - 1) & 0xffff);
 	m_data_addr_low = (unsigned char)((m_data_addr) & 0x00ff);
--- CVDmanager/ModbusSocket.h
+++ CVDmanager/ModbusSocket.h
@@ -15,26 +15,25 @@ private:
 	QString ip;
 	int port;
 	
-	unsigned char m_1;
-	unsigned char m_2;
-	unsigned char m_3;
-	unsigned char m_4;
-	unsigned char m_7;
+	unsigned char m_1 = 0x00;
+	unsigned char m_2 = 0x00;
+	unsigned char m_3 = 0x00;
+	unsigned char m_4 = 0x00;
+	unsigned char m_7 = 0x00;
 
 	long m_data_addr;
-	unsigned char m_data_addr_high;
-	unsigned char m_data_addr_low;
+	unsigned char m_data_addr_high = 0x00;
+	unsigned char m_data_addr_low = 0x00;
 	long m_data_num;
-	unsigned char m_data_num_high;
-	unsigned char m_data_num_low;
+	unsigned char m_data_num_high = 0x00;
+	unsigned char m_data_num_low = 0x00;
 	long m_data;
 	int flag;
 
 public:
 
-	ModbusSocket(unsigned char m_1, unsigned char m_2, unsigned char m_3,
-		unsigned char m_4, unsigned char m_7, unsigned char m_data_addr,
-		unsigned char m_data_num);
+	ModbusSocket(std::string device_name);
+
 	~ModbusSocket();
 
 	void SendModbusData();
